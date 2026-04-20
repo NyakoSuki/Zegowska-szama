47cbc704dbc68864e6ac5bc139306f8c5c518d40
Kamil60094 <u.kamil4321@gmail.com>
2026-04-20 17:16:16 +0200

    zmiana php do animacji

--- files/login-signup/login-signup.php
+++ files/login-signup/login-signup.php
@@ -9,51 +9,85 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logowanie/Rejestracja - Zegowska Szama</title>
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
     <main class="container-fluid">
-        <div class="col-12 vh-100 d-flex flex-column flex-md-row justify-content-center align-items-center">
-            <span class="in">
-                <h3><b>LOGIN</b></h3>
-                <form action="login.php" method="post">
-                    <label for="emailLogin">Email:<input id="emailLogin" name="emailLogin" type="text" placeholder="eamil" required></label>
-                    <label for="passwordLogin">Password:<input id="passwordLogin" name="passwordLogin" type="password" placeholder="password" required></label>
-                    <?php
-                        if(isset($_SESSION['correctData']) && $_SESSION['correctData'] === false)
-                            {
+        <img class="img-fluid" src="../../images/zegowska_szama_logo.png" alt="logo">
+        <div class="container-box">
+
+            <!-- PRZYCISKI PRZEŁĄCZANIA -->
+            <div class="switcher">
+                <button id="signinBtn" type="button">Login</button>
+                <button id="signupBtn" type="button">Sign Up</button>
+            </div>
+
+            <!-- SLIDER -->
+            <div class="slider" id="slider">
+
+                <!-- LOGIN -->
+                <div class="panel login">
+                    <h3><b>LOGIN</b></h3>
+
+                    <form action="login.php" method="post">
+                        <label>Email:<br>
+                            <input name="emailLogin" type="text" required>
+                        </label>
+
+                        <label>Password:<br>
+                            <input name="passwordLogin" type="password" required>
+                        </label>
+
+                        <?php
+                            if(isset($_SESSION['correctData']) && $_SESSION['correctData'] === false){
                                 echo "<p>Niepoprawny login lub hasło!</p>";
                                 unset($_SESSION['correctData']);
                             }
-                    ?>
-                    <div class="buttonsa">
-                        <button type="submit">Login</button>
-                        <button type="reset">reset</button>
-                    </div>
-                </form>
-            </span>
-            <span class="up">
-                <h3><b>SIGN UP</b></h3>
-                    <form action="signup.php" method="post">
-                        <label for="usernameSignup">Username:<input id="usernameSignup" name="usernameSignup" type="text" placeholder="username" required></label>
-                        <label for="emailSignup">Email:<input id="emailSignup" name="emailSignup" type="text" placeholder="email" required></label>
-                        <label for="passwordSignup">Password:<input id="passwordSignup" name="passwordSignup" type="password" placeholder="password" required></label>
-                         <?php
-                            if(isset($_SESSION['userExists']) && $_SESSION['userExists'] === true)
-                                {
-                                    echo "<p>Użytkownik już istnieje!</p>";
-                                    unset($_SESSION['userExists']);
-                                }
                         ?>
-                        <div class="buttonsb">
-                            <button type="submit">Sign up</button>
-                            <button  type="reset">reset</button>
+
+                        <div class="buttons">
+                            <button type="submit">Login</button>
+                            <button type="reset">Reset</button>
                         </div>
                     </form>
-            </span>
+                </div>
+
+                <!-- SIGN UP -->
+                <div class="panel signup">
+                    <h3><b>SIGN UP</b></h3>
+
+                    <form action="signup.php" method="post">
+                        <label>Username:<br>
+                            <input name="usernameSignup" type="text" required>
+                        </label>
+
+                        <label>Email:
+                            <input name="emailSignup" type="text" required>
+                        </label>
+
+                        <label>Password:
+                            <input name="passwordSignup" type="password" required>
+                        </label>
+
+                        <?php
+                            if(isset($_SESSION['userExists']) && $_SESSION['userExists'] === true){
+                                echo "<p>Użytkownik już istnieje!</p>";
+                                unset($_SESSION['userExists']);
+                            }
+                        ?>
+
+                        <div class="buttons">
+                            <button type="submit">Sign up</button>
+                            <button type="reset">Reset</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
         </div>
     </main>
+    <script src="app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
