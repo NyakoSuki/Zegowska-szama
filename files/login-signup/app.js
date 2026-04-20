4d3670d5b75c268c56f24670aec33505033cd7b2
Kamil60094 <u.kamil4321@gmail.com>
2026-04-20 17:36:14 +0200

    dodanie Javascript do animacji

---
+++ files/login-signup/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const signinBtn = document.getElementById("signinBtn");
+    const signupBtn = document.getElementById("signupBtn");
+    const container = document.querySelector(".container-box");
+
+    signinBtn.addEventListener("click", () => {
+        container.classList.remove("signup-active");
+    });
+
+    signupBtn.addEventListener("click", () => {
+        container.classList.add("signup-active");
+    });
+
+});
