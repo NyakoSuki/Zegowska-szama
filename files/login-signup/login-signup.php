47c6600df5d3d8d6a53775cb69003f3ccab2051e
Nyako suki <maksosmalak@gmail.com>
2026-04-20 15:50:33 +0200

    testy z nowym stylem

--- files/login-signup/login-signup.php
+++ files/login-signup/login-signup.php
@@ -9,18 +9,17 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logowanie/Rejestracja - Zegowska Szama</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
     <main class="container-fluid">
-        <img class="img-fluid" src="../../images/zegowska_szama_logo.png" alt="logo">
         <div class="col-12 vh-100 d-flex flex-column flex-md-row justify-content-center align-items-center">
             <span class="in">
                 <h3><b>LOGIN</b></h3>
                 <form action="login.php" method="post">
-                    <label for="emailLogin">Email:<input id="emailLogin" name="emailLogin" type="text" required></label>
-                    <label for="passwordLogin">password:<input id="passwordLogin" name="passwordLogin" type="password" required></label>
+                    <label for="emailLogin">Email:<input id="emailLogin" name="emailLogin" type="text" placeholder="eamil" required></label>
+                    <label for="passwordLogin">Password:<input id="passwordLogin" name="passwordLogin" type="password" placeholder="password" required></label>
                     <?php
                         if(isset($_SESSION['correctData']) && $_SESSION['correctData'] === false)
                             {
@@ -37,9 +36,9 @@ session_start();
             <span class="up">
                 <h3><b>SIGN UP</b></h3>
                     <form action="signup.php" method="post">
-                        <label for="usernameSignup">Username:<input id="usernameSignup" name="usernameSignup" type="text" required></label>
-                        <label for="emailSignup">Email:<input id="emailSignup" name="emailSignup" type="text" required></label>
-                        <label for="passwordSignup">password:<input id="passwordSignup" name="passwordSignup" type="password" required></label>
+                        <label for="usernameSignup">Username:<input id="usernameSignup" name="usernameSignup" type="text" placeholder="username" required></label>
+                        <label for="emailSignup">Email:<input id="emailSignup" name="emailSignup" type="text" placeholder="email" required></label>
+                        <label for="passwordSignup">Password:<input id="passwordSignup" name="passwordSignup" type="password" placeholder="password" required></label>
                          <?php
                             if(isset($_SESSION['userExists']) && $_SESSION['userExists'] === true)
                                 {
