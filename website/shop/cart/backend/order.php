31db5e52398b6f08f690c51151bdae8dd29469f7
Nyako suki <maksosmalak@gmail.com>
2026-04-28 09:57:01 +0200

    dadane zamawianie produktów

--- website/shop/cart/backend/order.php
+++ website/shop/cart/backend/order.php
@@ -5,50 +5,90 @@ require_once BLOCKER_PATH;
 
 include DB_PATH;
 
-
-$userId = $_SESSION["id"];
-$totalPrice = 
+$connection->begin_transaction();
 
 
-$stmt = $connection->prepare
-        ("
-        INSERT INTO orders (user_id = ?, total_price = ?)
-        VALUES (?, ?)
-        ");
-
-        if (!$stmt) exit("SQL prepare error");
-
-        $stmt->bind_param
-        (
-            "sd",
-            $userId,
-            $
-        );
-
-    if (!$stmt->execute()) exit("SQL execute error");
-
-while()
+try
 {
+    $userId = $_SESSION["id"];
+    $totalPrice = 0;
+    $cart = $_SESSION['cart'] ?? [];
+
+    if (empty($cart)) throw new Exception("SQL cart error");
+
+    $counts = array_count_values($cart);
+
+
+    $stmt = $connection->prepare("SELECT price FROM products WHERE id = ?");
+
+    if (!$stmt) throw new Exception("SQL prepare error");
+
+    foreach ($counts as $id => $qty)
+    {
+        $stmt->bind_param("i", $id);
+
+        if (!$stmt->execute()) throw new Exception("SQL execute error");
+
+        $result = $stmt->get_result();
+
+        $product = $result->fetch_assoc();
+        if (!$product) throw new Exception("Product not found");
+
+        $totalPrice += $product["price"] * $qty;
+    }
+
+
     $stmt = $connection->prepare
     ("
-    INSERT INTO ordered_products (user_id = ?, product_id = ?, quantity = ?)
-    VALUES (?, ?, ?)
+    INSERT INTO orders (user_id, total_price)
+    VALUES (?, ?)
     ");
-
-    if (!$stmt) exit("SQL prepare error");
+    
+    if (!$stmt) throw new Exception("SQL prepare error");
 
     $stmt->bind_param
     (
-        "ssi",
+        "id",
         $userId,
-        $,
-        $
+        $totalPrice
     );
 
-    if (!$stmt->execute()) exit("SQL execute error");
-}
+    if (!$stmt->execute()) throw new Exception("SQL execute error");
+
+    
+    $orderId = $connection->insert_id;
+
+    $stmt = $connection->prepare
+    ("
+    INSERT INTO ordered_products (order_id, product_id, quantity)
+    VALUES (?, ?, ?)
+    ");
+
+    if (!$stmt) throw new Exception("SQL prepare error");
+
+    foreach ($counts as $id => $qty) 
+    {
+        $stmt->bind_param
+        (
+            "iii",
+            $orderId,
+            $id,
+            $qty
+        );
+
+        if (!$stmt->execute()) throw new Exception("SQL execute error");
+    }
 
 
+    $connection->commit();
 
+    $_SESSION['cart'] = [];
     header("Location: " . CART_F_URL . "cart.php");
     exit;
+}
+
+catch (Exception $e) 
+{
+    $connection->rollback();
+    exit("Transaction failed");
+}
