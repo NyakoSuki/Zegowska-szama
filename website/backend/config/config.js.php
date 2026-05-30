68749ae5ca4050c02291285b856f3dcdd1a3a293
Kamil60094 <u.kamil4321@gmail.com>
2026-05-30 20:05:56 +0200

    komentarze config.js.php

--- website/backend/config/config.js.php
+++ website/backend/config/config.js.php
@@ -1,10 +1,11 @@
-<?php require_once __DIR__ . "/config.php"?>
+<?php require_once __DIR__ . "/config.php" ?>
 
 <script>
+// Code responsible for URL configuration in JavaScript
 window.CONFIG =
 {
-    BASE_URL: "<?= BASE_URL ?>",
+    BASE_URL:    "<?= BASE_URL ?>",
     BACKEND_URL: "<?= BACKEND_URL ?>",
-    PUBLIC_URL: "<?= PUBLIC_URL ?>"
+    PUBLIC_URL:  "<?= PUBLIC_URL ?>"
 };
 </script>
