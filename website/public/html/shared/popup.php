15bd1c460c9bf86af7361b1420267b807e97e296
Kamil60094 <u.kamil4321@gmail.com>
2026-05-30 20:23:24 +0200

    shared popup

--- website/public/html/shared/popup.php
+++ website/public/html/shared/popup.php
@@ -1,45 +1,114 @@
-<!--
-< ====================CART INFO====================
- -> showes up in corner and gives user appropriate response
-< ==================================================
--->
-<div
-    class="toast-container position-fixed bottom-0 end-0 p-3"
+<header
+    class="container-fluid sticky-top p-3"
 >
     <div
-        id="cartAddToast"
-        class="toast border-0 shadow"
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
+        class="top row align-items-center"
     >
+        <!-- School logo – hidden on mobile, links to school website -->
         <div
-            class="toast-header border-0"
+            class="col-sm-3 col-9 mb-2 d-sm-flex d-none justify-content-sm-end justify-content-start"
         >
-            <strong
-                class="me-auto fw-bold"
+            <a 
+                href="https://www.zs4.oswiata.tychy.pl/"
+                class="d-inline-block shadow-none"
             >
-                Koszyk
-            </strong>
+                <img 
+                    src="<?=PUBLIC_URL?>img/logo.svg"
+                    class="img-fluid img-logo" alt="logo"
+                >
+            </a>
+        </div>
+
+        <!-- App logo – hidden below lg, links to shop -->
+        <div
+            class="col-lg-5 col-0 mb-2 d-lg-flex d-none justify-content-md-start"
+        >
+            <a 
+                href="<?=PUBLIC_URL?>html/shop/shop.php"
+                class="d-inline-block shadow-none"
+            >
+                <img 
+                    src="<?=PUBLIC_URL?>img/zegowska-szama.svg"
+                    class="img-fluid img-logo" alt="zegowska-szama"
+                >
+            </a>
+        </div>
+
+        <div
+            class="nav col-lg-4 col-sm-9 col-12 d-flex justify-content-sm-end justify-content-center gap-2"
+        >
+
+            <!-- Show admin panel button when inside the admin folder, otherwise show account link -->
+            <?php if (isset($folder) && $folder === "admin"): ?>
+
+            
+                href="<?=PUBLIC_URL?>html/admin/admin.php"
+                class="btn btn-secondary"
+            >
+                <img
+                    class="naviImg"
+                    src="<?=PUBLIC_URL?>img/panel.svg"
+                    alt="admin panel"
+                >
+            </a>
+
+            <?php else: ?>
+
+            
+                href="<?=PUBLIC_URL?>html/account/account.php"
+                class="btn btn-secondary"
+            >
+                <img
+                    class="naviImg"
+                    src="<?=PUBLIC_URL?>img/account.svg"
+                    alt="Konto"
+                >
+            </a>
+
+            <?php endif; ?>
+
+            <!-- Cart link – always visible -->
+            
+                href="<?=PUBLIC_URL?>html/cart/cart.php"
+                class="btn btn-secondary"
+            >
+                <img
+                    class="naviImg"
+                    src="<?=PUBLIC_URL?>img/cart.svg"
+                    alt="Koszyk"
+                >
+            </a>
+
+            <!-- Show filter toggle button on shop page and all admin pages, otherwise show home link -->
+            <?php if ($site === "shop.php" || (isset($folder) && $folder === "admin")): ?>
 
             <button
                 type="button"
-                class="btn-close"
-                data-bs-dismiss="toast"
-                aria-label="Close"
+                id="filterBtn"
+                class="btn btn-secondary"
             >
+                <img
+                    class="naviImg"
+                    src="<?=PUBLIC_URL?>img/menu.svg"
+                    alt="Filtry"
+                >
             </button>
-        </div>
 
-        <div
-            class="toast-body"
-        >
-            <p
-                id="addToCartResult"
-                name=""
-                class="h6 mb-0"
+            <?php else: ?>
+
+            
+                href="<?=PUBLIC_URL?>html/shop/shop.php"
+                class="btn btn-secondary"
             >
-            </p>
+                <img
+                    class="naviImg"
+                    src="<?=PUBLIC_URL?>img/home.svg"
+                    alt="Sklep"
+                >
+            </a>
+
+            <?php endif; ?>
+            
         </div>
     </div>
-</div>
+</header>
