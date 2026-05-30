e18aba04728cb38d26450a62848797e9966a66d4
Kamil60094 <u.kamil4321@gmail.com>
2026-05-30 20:08:02 +0200

    komentarze do siteblocker

--- website/backend/shared/siteblocker.php
+++ website/backend/shared/siteblocker.php
@@ -1,6 +1,7 @@
 <?php
 require_once dirname(__DIR__, 2) . "/backend/config/config.php";
 
+// Block access to this file if the user is not logged in
 if (!isset($_SESSION["id"])) 
 {
     header("Location: " . PUBLIC_URL . "html/auth/auth.php");
